548fb5b763c67c7186e66cd9460903f881285ae5
andrey <19ofis96@mail.ru>
2017-09-05 11:07:23 +0300

    privatmarket

---
+++ catalog/controller/feed/privat_market.php
@@ -0,0 +1,126 @@
+<?php
+class ControllerFeedPrivatMarket extends Controller {
+
+    public function index(){
+        $this->load->model('feed/privat_market');
+        $array = $this->model_feed_privat_market->getBudivaProducts();
+//        echo count($array['categories']);die;
+        $this->createXML($array);
+//        echo "<pre>"; print_r($array); echo "</pre>";die;
+    }
+
+    public function createXML($array){
+        $dom = new DOMDocument("1.0", "utf-8");
+
+        $root = $dom->createElement('yml_catalog');
+
+        $shop = $dom->createElement('shop');
+        $shop->appendChild($dom->createElement('name',"ТОВ 'БУДІВА'"));
+        $shop->appendChild($dom->createElement('company',"ТОВ 'БУДІВА'"));
+        $shop->appendChild($dom->createElement('url',"budsite.ua"));
+
+        $currencies = $dom->createElement('currencies');
+        $currency = $dom->createElement('currency');
+        $currency->setAttribute('id', 'UAH');
+        $currency->setAttribute('rate', 1);
+
+
+        $currencies->appendChild($currency);
+        $shop->appendChild($currencies);
+
+        $categories = $dom->createElement('categories');
+
+        $i=1;
+        foreach ($array['categories'] as $category_self) {
+
+            $category = $dom->createElement('category');
+            $category->nodeValue = $category_self;
+            $category->setAttribute('id', $i);
+            $i++;
+
+            $categories->appendChild($category);
+        }
+
+
+        $shop->appendChild($categories);
+
+        $offers = $dom->createElement('offers');
+
+        foreach ($array['products'] as $product_self){
+            $offer = $dom->createElement('offer');
+            $offer->setAttribute('available', 'true');
+
+            $name = $dom->createElement('name');
+            $name->nodeValue = $product_self['model'];
+            $offer->appendChild($name);
+
+            $category_product = $dom->createElement('categoryId');
+            $key = array_search($product_self['name'], $array['categories']);
+            $category_product->nodeValue = $key+1;
+            $offer->appendChild($category_product);
+
+            $price = $dom->createElement('price');
+            $price->nodeValue = $product_self['price'];
+            $offer->appendChild($price);
+
+            $currency_id = $dom->createElement('currencyId');
+            $currency_id->nodeValue = 'UAH';
+            $offer->appendChild($currency_id);
+
+            $picture = $dom->createElement('picture');
+            $picture->nodeValue = 'https://budsite.ua/image/' . $product_self['image'];
+            $offer->appendChild($picture);
+
+            $description = $dom->createElement('description');
+            $description->nodeValue = $product_self['description'];
+            $offer->appendChild($description);
+
+            $attributes = explode('; ', $product_self['attributes']);
+            $attributes_values = explode('; ', $product_self['attributes_values']);
+            $count_attributes = count($attributes_values);
+
+            for ($i=0; $i<$count_attributes; $i++){
+
+                $r='![,]!';
+                if (preg_match($r, $attributes[$i])){
+                    $attributes_explode = explode( ', ' , $attributes[$i]);
+//                    echo "<pre>"; print_r($attributes_explode); echo "</pre>";die;
+                    $param_xml = $dom->createElement('param');
+                    $param_xml->setAttribute('name', $attributes_explode[0]);
+                    $param_xml->setAttribute('unit', $attributes_explode[1]);
+                }else{
+                    $param_xml = $dom->createElement('param');
+                    $param_xml->setAttribute('name', $attributes[$i]);
+
+                }
+
+
+                $param_xml->nodeValue = $attributes_values[$i];
+                $offer->appendChild($param_xml);
+            }
+//            echo "<pre>"; print_r($attributes); echo "</pre>";
+//            echo "<pre>"; print_r($attributes_values); echo "</pre>";die;
+
+
+
+            $delivery = $dom->createElement('delivery');
+            $delivery->nodeValue = 'true';
+            $offer->appendChild($delivery);
+
+            $offers->appendChild($offer);
+        }
+
+        $shop->appendChild($offers);
+
+        $root->appendChild($shop);
+        $dom->appendChild($root);
+
+//        $dom->appendChild($dom->createElement('shop'));
+        header('Content-type: text/xml');
+        header('Pragma: public');
+        header('Cache-control: private');
+        header('Expires: -1');
+        echo $dom->saveXML();
+//        echo "<pre>"; print_r($dom); echo "</pre>";
+    }
+}
---
+++ catalog/model/feed/privat_market.php
@@ -0,0 +1,46 @@
+<?php
+class ModelFeedPrivatMarket extends Model {
+
+    /** Метод выборки товаров из бд для импорта в ПриватМаркет
+     *
+     * @return array [0] => Категории товаров, [1] => Товары с характеристиками
+     *
+     */
+    public function getBudivaProducts(){
+        $products = $this->db->query("SELECT p.product_id, p.model, p.image, p.price, cd.name AS category_name,
+         GROUP_CONCAT(DISTINCT ad.name SEPARATOR '; ') AS attributes, GROUP_CONCAT(DISTINCT pa.text SEPARATOR '; ') AS attributes_values,  pd.description  FROM ". DB_PREFIX ."product AS p
+         LEFT JOIN ". DB_PREFIX ."product_description AS pd ON pd.product_id = p.product_id 
+         LEFT JOIN ". DB_PREFIX ."product_to_category AS p2c ON p.product_id = p2c.product_id
+         LEFT JOIN ". DB_PREFIX ."category_description AS cd ON p2c.category_id = cd.category_id
+         LEFT JOIN ". DB_PREFIX ."product_attribute AS pa ON p.product_id = pa.product_id
+         LEFT JOIN ". DB_PREFIX ."attribute_description AS ad ON pa.attribute_id = ad.attribute_id 
+         where manufacturer_id='74'
+         OR manufacturer_id='69'
+         OR manufacturer_id='66'
+         OR manufacturer_id='54'
+         OR manufacturer_id='71'
+         OR manufacturer_id='63'
+         OR manufacturer_id='58'
+         OR manufacturer_id='51'
+         OR manufacturer_id='65'
+         OR manufacturer_id='64'
+         OR manufacturer_id='40'
+         OR manufacturer_id='48'
+         OR manufacturer_id='89' GROUP BY p.product_id");
+
+        $categories_pre = [];
+        foreach ($products->rows as $product){
+            $categories_pre[] = $product['name'];
+        }
+
+        $categories_pre = array_unique($categories_pre);
+        $categories = [];
+        foreach ($categories_pre as $category){
+            $categories[] = $category;
+        }
+//        echo "<pre>"; print_r($products); echo "</pre>";die;
+        return array('categories' => $categories, 'products' => $products->rows);
+    }
+}
+
+// LEFT JOIN ". DB_PREFIX ."attribute_description AS ad ON pa.attribute_id = ad.attribute_id / pa.product_id  ad.name, pd.description
