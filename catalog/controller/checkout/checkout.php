fa54575f547342f45026bb65cf8eea467099627d
andrey <19ofis96@mail.ru>
2017-09-11 15:13:40 +0300

    trying fix mix content

--- catalog/controller/checkout/checkout.php
+++ catalog/controller/checkout/checkout.php
@@ -23,8 +23,8 @@ class ControllerCheckoutCheckout extends Controller {
 
     $total_data = array();
     $total = 0;
-    header("Content-Security-Policy: default-src 'self'");
-    header("Content-Security-Policy: script-src http://www.delivery-auto.com");
+//    header("Content-Security-Policy: default-src 'self'");
+    header("Content-Security-Policy: script-src 'self' http://www.delivery-auto.com");
     unset($this->session->data['privat_html']);
 
     $taxes = $this->cart->getTaxes();
