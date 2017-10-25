f78ca889bffc402880742750e0e06b23fc5280e4
andrey <19ofis96@mail.ru>
2017-10-25 15:42:46 +0300

    initial commit

---
+++ admin/language/russian/module/dka_cart.php
@@ -0,0 +1,141 @@
+<?php
+// Heading
+$_['heading_title'] = 'DKA Cart';
+
+$_['text_default']        = 'Главная';
+$_['text_table_cart']     = 'Список товаров не оформленных в заказ';
+$_['text_save']           = 'Сохранить';
+$_['text_error']          = 'Ошибка';
+$_['entry_settings_info'] = 'Настройки модуля';
+$_['text_export']         = 'Экспорт';
+$_['text_module']      		= 'Модули';
+$_['text_arhive_process'] = 'Архивация';
+$_['text_download']       = 'Скачать';
+$_['text_input_search']   = 'Поиск';
+$_['text_clear']          = 'Очистить';
+$_['text_confirm']        = 'Данное действие необратимо. Вы уверены?';
+$_['text_back']           = 'Назад';
+$_['text_forward']        = 'Вперед';
+$_['text_loading']        = 'Загрузка';
+$_['text_empty']          = 'Отсутствуют';
+$_['text_close']          = 'Закрыть';
+$_['text_noshow']         = 'Больше не показывать';
+// STARTUP
+$_['text_startup_header']     = 'Спасибо за установку';
+$_['text_startup_free']       = 'Бесплатно';
+$_['text_startup_free_p']     = 'Вы можете использовать данный модуль совершенно бесплатно, за исключением использования исходного кода для перепродажи и использования в своих продуктах, продажа модуля категорически запрещена. (Исходный код и дистрибутив принадлежит правообладателю)';
+$_['text_startup_addition']   = 'Дополнительно';
+$_['text_startup_addition_p'] = 'Для обладателей лицензии на модуль DKA Export.Import в модуле доступна дополнительная возможность "Экспорт корзин в Excel". Необходима минимальная версия 2.0.1';
+
+$_['text_startup_contact'] = 'Контакты: Dream K & A Develop';
+$_['text_startup_sale']    = 'Сайт продаж:';
+$_['text_startup_sale_of'] = 'Официальный сайт продаж:';
+$_['text_startup_develop'] = 'Сайт разработок:';
+$_['text_startup_support'] = 'Техническая поддержка:';
+
+$_['text_noshow_herror'] = 'Ошибка отлючения стартового окна';
+$_['text_noshow_perror'] = 'При попытке отключить стартовое окно возникла ошибка, прочитайте об ошибки подробнее в дополнительном описании.';
+// DEFAULT
+$_['tooltip_user']       = 'Перейти к профилю пользователя';
+$_['tooltip_product']    = 'Перейти к редактированию товара';
+$_['tooltip_delete']     = 'Удалить товар из корзины пользователя';
+$_['tooltip_delete_all'] = 'Очистить таблицу с корзинами';
+
+$_['text_pagi_view'] = 'Показано с';
+$_['text_pagi_by']   = 'по';
+$_['text_pagi_from'] = 'из';
+$_['text_pagi_page'] = 'страниц';
+
+$_['text_data_null'] = 'Данные отсутствуют';
+
+$_['text_search_hinfo_p'] = 'Ограничение на кол-во символов';
+$_['text_search_pinfo_p'] = 'Минимальное кол-во символов в строке поиска 3 (три)';
+
+$_['text_del_data_hinfo'] = 'Ошибка удаления данных';
+$_['text_del_data_pinfo'] = 'При попытке удалить значения из таблицы возникла ошибка, прочитайте об ошибки подробнее в дополнительном описании.';
+
+$_['text_load_data_hinfo'] = 'Ошибка загрузки данных';
+$_['text_load_data_pinfo'] = 'При попытке получить значения из таблицы возникла ошибка, прочитайте об ошибки подробнее в дополнительном описании.';
+// TABLE CART
+$_['text_th_image']  = 'Изображение';
+$_['text_th_name']   = 'Наименование товара';
+$_['text_th_fio']    = 'ФИО';
+$_['text_th_email']  = 'E-mail';
+$_['text_th_phone']  = 'Телефон';
+$_['text_th_date']   = 'Дата создания';
+$_['text_th_action'] = 'Действие';
+// EXPORT
+$_['tooltip_export_carts']   = 'Экспортировать список не оформленных заказок';
+$_['text_export_h_base']     = 'Экспорт данных...';
+$_['text_finished']          = 'Завершен';
+$_['text_export_t_files']    = 'Экспортировано файлов';
+$_['text_export_t_all_load'] = 'Скачать одним архивом';
+
+$_['text_export_th_name']     = 'Наименование файла';
+$_['text_export_th_count']    = 'Количество';
+$_['text_export_th_size']     = 'Размер файла';
+$_['text_export_th_download'] = 'Ссылка для скачивания';
+
+$_['text_export_t_atbase'] = 'В базе данных';
+
+$_['text_export_herror'] = 'Ошибка экспорта';
+$_['text_export_perror'] = 'Возникла ошибка при попытке экспорта данных в файл.';
+
+$_['text_export_zip_herror'] = 'Ошибка архивации файлов';
+$_['text_export_zip_perror'] = 'Возникла ошибка при попытки добавить файлы в архив.';
+// FOOTER
+$_['text_footer_site']    = 'Домашняя страница проекта';
+$_['text_footer_twitter'] = 'Мы ждем вас в Twitter';
+$_['text_footer_youtube'] = 'Канал на YouTube';
+$_['text_footer_vk']      = 'Страница в социальных сетях';
+
+$_['text_footer_support'] = 'Написать в техническую поддержку';
+// MAIL
+$_['text_mail_modal_send']    = 'Отправить';
+$_['text_mail_modal_close']   = 'Закрыть';
+$_['text_mail_modal_address'] = 'Кому';
+$_['text_mail_modal_answer']  = 'Адрес для ответа';
+$_['text_mail_modal_copy']    = 'Копия';
+$_['text_mail_modal_subject'] = 'Тема';
+$_['text_mail_modal_message'] = 'Сообщение';
+
+$_['tooltip_mail_user']              = 'Написать сообщение клиенту';
+$_['text_mail_header']               = 'Направить сообщение клиенту';
+$_['text_mail_support_header']       = 'Обращение в техническую поддержку';
+$_['text_mail_support_subject_send'] = 'Запрос технической поддержки';
+$_['text_mail_subject_send']         = 'Предлагаем Вам ознакомиться с предложением';
+$_['text_mail_modal_message_url']    = 'Адрес на котором наблюдается проблема:';
+
+$_['text_mail_modal_header_forward'] = 'Сообщение отправляется...';
+$_['text_mail_modal_header_send']    = 'Сообщение успешно отправлено';
+$_['text_mail_modal_header_error']   = 'Ошибка отправки';
+
+$_['text_mail_herror'] = 'Ошибка отправки сообщения';
+$_['text_mail_perror'] = 'При попытке отправить сообщение возникла ошибка, прочитайте об ошибки подробнее в дополнительном описании.';
+// ALERT
+$_['tooltip_alert_mail_send'] = 'Отправить';
+$_['text_alert_mail_send']    = 'Отправить в поддержку';
+// SETTINGS
+$_['text_settings_save_hinfo_p'] = 'Ошибка сохранения настроек';
+$_['text_settings_save_pinfo_p'] = 'Возникла ошибка при сохранить настройки.';
+
+$_['text_settings_modal_h'] = 'Настройки модуля';
+
+$_['text_settings_update_copyright']  = 'Обновление';
+$_['text_settings_update_check']      = 'Проверка обновления';
+$_['text_settings_update_check']      = 'Проверить';
+$_['text_settings_update_show_modal'] = 'Отображать обновление в модальном окне';
+// UPDATE
+$_['text_update_check']          = 'Проверить';
+$_['text_update_header_new']     = 'Новая версия модуля';
+$_['text_update_subheader_new']  = 'Вы можете обновиться до последней версии модуля';
+$_['text_update_subheader_date'] = 'Дата релиза';
+$_['text_update_header_info']    = 'Информация';
+$_['text_update_header_change']  = 'Изменения';
+$_['text_update_forward_load']   = 'Перейти на страницу загрузки модуля';
+$_['text_update_no']             = 'Не найдено';
+$_['text_update_mr_client']      = 'Уважаемый клиент';
+
+$_['text_update_herror']        = 'Ошибка проверки обновления';
+$_['text_update_perror']        = 'При попытке получить информацию об обновлении модуля возникла ошибка, прочитайте об ошибки подробнее в дополнительном описании.';
+?>
