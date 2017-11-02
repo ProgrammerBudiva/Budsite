e8b1ed5a355c617d811bdd19198d9102944dda22
andrey <19ofis96@mail.ru>
2017-11-02 10:16:02 +0200

    dka cart

--- admin/view/javascript/ckeditor/config.js
+++ admin/view/javascript/ckeditor/config.js
@@ -7,4 +7,25 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+    config.extraPlugins = 'youtube';
+    // config.toolbar = [{ name: 'insert', items:['Image', 'Youtube']}];
+    config.youtube_responsive = true;
+    config.youtube_controls = true;
+
+
+    /* Filebrowser routes */
+
+    // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Link tab of Image dialog.
+    // config.filebrowserImageBrowseLinkUrl = "/admin/index.php?route=common/filemanager&ckedialog=info:txtUrl";
+
+    // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Image dialog.
+    // config.filebrowserImageBrowseUrl = "/admin/index.php?route=common/filemanager&ckedialog=info:txtUrl";
+
+    // The location of a script that handles file uploads in the Image dialog.
+    // config.filebrowserImageUploadUrl = "/admin/index.php?route=common/filemanager&ckedialog=info:txtUrl";
+
+
+    config.allowedContent = true;
+
+
 };
