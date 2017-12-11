5d63e7cfd3f39ba1622e1ed1f845f94260edafcd
andrey <19ofis96@mail.ru>
2017-12-11 17:28:27 +0200

    db escaping

--- catalog/model/checkout/oneclick.php
+++ catalog/model/checkout/oneclick.php
@@ -2,6 +2,7 @@
 class ModelCheckoutOneclick extends Model {
     function add($phone, $order){
         $date = date('Y-m-d h:i:s', time());
+        $order = mysql_escape_string($order);
         $test = $this->db->query("INSERT INTO " . DB_PREFIX . "oneclick SET phone = '" . $phone . "', `date` = '". $date ."' , `order` = '" . $order . "'");
     }
 
